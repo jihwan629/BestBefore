1fe6965d7c7998f27450edd3480a447636d43696
jihwan629 <jihwan629@naver.com>
2021-06-11 21:33:49 +0900

    하단 탭 만들기

---
+++ screens/ListScreen.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { 
+    Text,
+    StyleSheet,
+} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const ListScreen = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>
+                목록 화면
+            </Text>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
+
+export default ListScreen
