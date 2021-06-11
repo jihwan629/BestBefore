8160151a7490356817cf673c25087f6c2677b7e5
jihwan629 <jihwan629@naver.com>
2021-06-11 23:47:56 +0900

    글 정보 부품

---
+++ components/ArticleItem.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { 
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+    Platform,
+} from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+
+const ArticleItem = ({
+    article: {
+        id,
+        image,
+        name,
+        date,
+    },
+}) => {
+    return (
+        <TouchableOpacity
+            activeOpacity={0.8}
+        >
+            <View style={styles.container}>
+                <View>
+                    <Image
+                        style={styles.image}
+                        source={image}
+                    />
+                </View>
+
+                <View style={styles.info}>
+                    <Text style={styles.date}>
+                        {date}
+                    </Text>
+                    <Text 
+                        style={styles.name}
+                        numberOfLines={2}
+                    >
+                        {name}
+                    </Text>
+                </View>
+            </View>
+            
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        padding: 16,
+        paddingBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#e2e2e2',
+    },
+    info: {
+        flex: 1,
+        paddingBottom: 16,
+    },
+    name: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#212121'
+    },
+    date: {
+        marginBottom: 8,
+        fontSize: 18,
+        color: '#9e9e9e'
+    },
+    image: {
+        width: 80,
+        height: 80,
+        marginRight: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: Platform.select({
+            ios: 1,
+            android: 4
+        }),
+    },
+})
+
+export default ArticleItem
--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -5,11 +5,30 @@ import {
 } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../components/Header'
+import ArticleItem from '../components/ArticleItem'
+
+let defaultimage = require('../assets/favicon.png');
 
 const ListScreen = () => {
     return (
         <SafeAreaView style={styles.container}>
             <Header title="목록" />
+            <ArticleItem
+                article={{
+                    id: 1,
+                    name: '딸기 샌드위치, 백종원 도시락, 전주 비빔밥 삼각김밥, 바나나, 사과, 오렌지, 트윈 쿠키, 돈가스 삼각김밥, 제육 김밥, 참치마요 삼각김밥, 피쉬 버거',
+                    date: '2021년 6월 10일',
+                    image: defaultimage,
+                }}
+            />
+            <ArticleItem
+                article={{
+                    id: 2,
+                    name: '제육 삼각김밥',
+                    date: '2021년 6월 11일',
+                    image: defaultimage,
+                }}
+            />
         </SafeAreaView>
     )
 }
