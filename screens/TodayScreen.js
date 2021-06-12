c1703e05edfba4d7ec5532a5ec505573509e91c0
jihwan629 <jihwan629@naver.com>
2021-06-13 02:18:09 +0900

    오늘 목록 화면

--- screens/TodayScreen.js
+++ screens/TodayScreen.js
@@ -5,11 +5,22 @@ import {
 } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../components/Header'
+import ArticleItem from '../components/ArticleItem'
+
+let defaultimage = require('../assets/favicon.png');
 
 const TodayScreen = () => {
     return (
         <SafeAreaView style={styles.container}>
             <Header title="오늘" />
+            <ArticleItem
+                article={{
+                    id: 2,
+                    name: '제육 삼각김밥',
+                    date: '2021년 6월 11일',
+                    image: defaultimage,
+                }}
+            />
         </SafeAreaView>
     )
 }
