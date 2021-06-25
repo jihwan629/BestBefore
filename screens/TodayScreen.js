9d01a51bc132b4e5d7df0625371180740e2e58c8
jihwan629 <jihwan629@naver.com>
2021-06-25 21:18:46 +0900

    지난, 오늘 폐기 모달창

--- screens/TodayScreen.js
+++ screens/TodayScreen.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { 
+    View,
     Text,
     StyleSheet,
     FlatList,
+    Modal,
+    TouchableOpacity,
 } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { withContext } from 'react-simplified-context'
@@ -13,13 +16,61 @@ const TodayScreen = ({
     articles,
     removeToday,
 }) => {
+    const [isModalVisible, setModalVisible] = useState(false)
+
+    const toggleModal = () => {
+        setModalVisible(!isModalVisible)
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <Header 
                 title="오늘" 
                 button="오늘 폐기 삭제"
-                onPress={() => removeToday()}
+                // onPress={() => removeToday()}
+                onPress={() => toggleModal()}
             />
+
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={isModalVisible}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>
+                            정말로 삭제 하시겠습니까?
+                        </Text>
+
+                        <View
+                            style={styles.containerButton}
+                        >
+                            <TouchableOpacity
+                                activeOpacity={0.8}
+                                style={{ ...styles.modalButton, backgroundColor: '#da5746' }}
+                                onPress={() => {
+                                    removeToday()
+                                    toggleModal()
+                                }}
+                            >
+                                <Text style={styles.textStyle}>
+                                    삭제
+                                </Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity
+                                activeOpacity={0.8}
+                                style={{ ...styles.modalButton, backgroundColor: '#9e9e9e' }}
+                                onPress={() => toggleModal()}
+                            >
+                                <Text style={styles.textStyle}>
+                                    취소
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
 
             <FlatList 
                 data={articles.filter((article) => {
@@ -46,7 +97,58 @@ const TodayScreen = ({
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    }
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: '#f2f2f2',
+        borderRadius: 10,
+        paddingTop: 35,
+        paddingBottom: 15,
+        paddingLeft: 10,
+        paddingRight: 10,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    containerButton: {
+        flexDirection: 'row',
+        padding: 5,
+    },
+    modalButton: {
+        backgroundColor: '#F194FF',
+        borderRadius: 10,
+        padding: 20,
+        paddingLeft: 40,
+        paddingRight: 40,
+        margin: 15,
+        marginBottom: 0,
+        elevation: 2,
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#212121',
+    },
 })
 
 export default withContext(TodayScreen)
